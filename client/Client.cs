a3edb338c76ac24a0b1009995cbf96ca3882299d
MatheusFEKK <matheusfelipelazarini4@gmail.com>
2025-05-17 11:53:09 -0300

    Adicionar arquivos de projeto.

---
+++ client/Client.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    internal class Client
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Insert the IP Address: ");
+
+            var port = Console.ReadLine();
+            var ipAddress = port.Substring(0, port.IndexOf(":"));
+            var portInserted = Convert.ToUInt64(port.Substring(port.IndexOf(":") + 1));
+
+            IPHostEntry localhost = await Dns.GetHostEntryAsync(ipAddress);
+            IPAddress localAddress = localhost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(localAddress, Convert.ToInt32(portInserted));
+            
+
+            Socket client = new Socket(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+
+            try
+            {
+                await client.ConnectAsync(ipEndPoint);
+               
+                while (client.Connected == true)
+                {
+                    Console.WriteLine("Connected with the server");
+    
+                    var command = Console.ReadLine().ToLower();
+                    if (command.ToString() == "disconnect")
+                    {
+                        client.Close();
+                        client.Disconnect(false);
+                        Console.WriteLine("Disconnect from server");
+                        Console.ReadKey();
+                        break;
+                    }
+                    if (command.ToString() == "message")
+                    {
+                        Console.WriteLine("Write your message to the server");
+                        try
+                        {
+                            var message = Console.ReadLine();
+                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                            ArraySegment<byte> bytes = new ArraySegment<byte>(messageBytes);
+                            await client.SendAsync(bytes, SocketFlags.None);
+                            Console.WriteLine("Message sended");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"{ex.Message}");
+                        }
+
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+
+            
+            
+
+        }
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Server
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            var hostName = Dns.GetHostName();
+            IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+            IPAddress localIpAddress = localhost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(localIpAddress, 11000);
+            Socket listener = new Socket(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp
+               );
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+            Console.WriteLine($"The server is listening on {Dns.GetHostAddresses(hostName)[2]}:{ipEndPoint.Port}");
+
+            var handler = await listener.AcceptAsync();
+
+
+            while (true)
+            {
+                Console.WriteLine($"User connected {localhost.AddressList.Count()}");
+
+                byte[] buffer = new byte[1024];
+                ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
+                var received = await handler.ReceiveAsync(segment, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                if (response != "")
+                {
+                    Console.WriteLine($"A message was received from the client: {response}");
+                }
+
+                var command = Console.ReadLine().ToLower();
+                if (command == "stop")
+                {
+                    listener.Close();
+                    handler.Close();
+                    Console.WriteLine("Server stopping...");
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
