f618cb27ad72279e57e82fe0b09dc52bb4605f28
MatheusFEKK <matheusfelipelazarini4@gmail.com>
2025-05-18 18:16:49 -0300

    Optimized the code and make feature of username

--- client/Client.cs
+++ client/Client.cs
@@ -12,50 +12,52 @@ namespace client
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Insert the IP Address: ");
+        string nickname = "";
+        string message;
+        Socket socket = null;
 
+            Console.WriteLine("Insert the IP Address and the Port: ");
             var port = Console.ReadLine();
             var ipAddress = port.Substring(0, port.IndexOf(":"));
             var portInserted = Convert.ToUInt64(port.Substring(port.IndexOf(":") + 1));
-
-            IPHostEntry localhost = await Dns.GetHostEntryAsync(ipAddress);
-            IPAddress localAddress = localhost.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(localAddress, Convert.ToInt32(portInserted));
-            
-
-            Socket client = new Socket(
-                ipEndPoint.AddressFamily,
-                SocketType.Stream,
-                ProtocolType.Tcp
-                );
+            socket = await ConnectToServer(ipAddress, portInserted);
 
             try
             {
-                await client.ConnectAsync(ipEndPoint);
-               
-                while (client.Connected == true)
+                if (socket != null)
                 {
                     Console.WriteLine("Connected with the server");
-    
+
+                }
+               
+                while (true)
+                {
+                    if (nickname == "")
+                    {
+                            Console.WriteLine("Insert your username: ");
+                            nickname = Console.ReadLine();
+                            string formatedNickname = $"Username: {nickname}";
+                            await SendMessage(socket, formatedNickname);
+                            Console.WriteLine("Username sended");  
+                    }
                     var command = Console.ReadLine().ToLower();
+
                     if (command.ToString() == "disconnect")
                     {
-                        client.Close();
-                        client.Disconnect(false);
-                        Console.WriteLine("Disconnect from server");
-                        Console.ReadKey();
+                        
+                        Console.WriteLine("Disconnecting from the server...");
                         break;
                     }
-                    if (command.ToString() == "message")
+                    if (command.ToString() == "message" && nickname != "")
                     {
                         Console.WriteLine("Write your message to the server");
                         try
                         {
-                            var message = Console.ReadLine();
-                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                            ArraySegment<byte> bytes = new ArraySegment<byte>(messageBytes);
-                            await client.SendAsync(bytes, SocketFlags.None);
+                            string templateMSG = $"Message: ";
+                            message = Console.ReadLine();
+                            await SendMessage(socket, templateMSG + message);
                             Console.WriteLine("Message sended");
+                            continue;
                         }
                         catch (Exception ex)
                         {
@@ -71,8 +73,35 @@ namespace client
                 Console.WriteLine($"{ex.Message}");
             }
 
+
+
+        }
+            static async Task<Socket> ConnectToServer(string ip, ulong port)
+        {
+            Console.WriteLine($"Connecting to {ip}:{port}...");
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), ((int)port));
+
+            Socket client = new Socket(
+                    ipEndPoint.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                    );
+
+            await client.ConnectAsync(ipEndPoint);
+
+            return client;
+
+
+        }
+
+        static async Task SendMessage(Socket socket, string message)
+        {
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes(message);
+
             
-            
+            ArraySegment<byte> bytes = new ArraySegment<byte>(data);
+            await socket.SendAsync(bytes, SocketFlags.None);
 
         }
     }
--- server/Server.cs
+++ server/Server.cs
@@ -5,19 +5,20 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace server
 {
     internal class Server
-    {
+    { 
         static async Task Main(string[] args)
         {
 
-
             var hostName = Dns.GetHostName();
             IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
-            IPAddress localIpAddress = localhost.AddressList[0];
+            IPAddress localIpAddress = IPAddress.Any;
             IPEndPoint ipEndPoint = new IPEndPoint(localIpAddress, 11000);
+
             Socket listener = new Socket(
                 ipEndPoint.AddressFamily,
                 SocketType.Stream,
@@ -26,41 +27,61 @@ namespace server
 
             listener.Bind(ipEndPoint);
             listener.Listen(100);
-            Console.WriteLine($"The server is listening on {Dns.GetHostAddresses(hostName)[2]}:{ipEndPoint.Port}");
 
-            var handler = await listener.AcceptAsync();
+            System.Net.IPAddress [] ips = new System.Net.IPAddress[7];
+
+            Console.WriteLine($"The server is listening on this ip's :");
+            for (var a = 0; a < Dns.GetHostAddresses(hostName).Length; a++)
+            {
+                Console.WriteLine(Dns.GetHostAddresses(hostName)[a]);
+                ips[a] = Dns.GetHostAddresses(hostName)[a];
+            }
+            Console.WriteLine($"The server is listening on this port : {ipEndPoint.Port}");
 
 
             while (true)
             {
-                Console.WriteLine($"User connected {localhost.AddressList.Count()}");
-
-                byte[] buffer = new byte[1024];
-                ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
-                var received = await handler.ReceiveAsync(segment, SocketFlags.None);
-                var response = Encoding.UTF8.GetString(buffer, 0, received);
-
-                if (response != "")
-                {
-                    Console.WriteLine($"A message was received from the client: {response}");
-                }
-
-                var command = Console.ReadLine().ToLower();
-                if (command == "stop")
-                {
-                    listener.Close();
-                    handler.Close();
-                    Console.WriteLine("Server stopping...");
-                }
+                var handler = await listener.AcceptAsync();
+                _ = UserHandler(handler);
+                Console.WriteLine($"User connected {handler.RemoteEndPoint}");
             }
+        }
+                
+                static async Task UserHandler(Socket handler)
+                {
+                    bool nicknameSet = false;
+                    string nickName = string.Empty;
+                    string message = string.Empty;
+                    byte[] buffer = new byte[1024];
 
+                while (true)
+                {
+                        ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
+                        int received = await handler.ReceiveAsync(segment, SocketFlags.None);
+                        var response = Encoding.UTF8.GetString(buffer, 0, received);
 
+                    if (response.StartsWith("Username:") && nicknameSet == false)
+                    {
+                        nickName = response.Substring(response.IndexOf(":") + 1);
+                        Console.WriteLine($"{nickName} joined the server");
+                        nicknameSet = true;
+                    }
 
+                    if (response.StartsWith("Message:"))
+                    {
+                        message = response.Substring(response.IndexOf(":") + 1);
+                        Console.WriteLine($"{nickName}: {message}");
+                    }
 
+                    if (received == 0 && nicknameSet == true)
+                    {
+                        Console.WriteLine($"{nickName} disconnected");
+                    }
 
-
+                }
 
 
         }
+            
     }
 }
